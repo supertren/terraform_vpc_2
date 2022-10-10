5e0857bb285b77952fa2dc35711eb084a5ebd0d5
supertren <supertren@hotmail.com>
2022-10-10 16:37:31 +0200

    comiteado Mon Oct 10 16:37:31 CEST 2022

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-   region = "us-east-1"
+  region = "us-east-1"
   shared_config_files      = ["/home/mik/.aws/config"]
   shared_credentials_files = ["/home/mik/.aws/credentials"]
   profile                  = "mik"
